e311bc175de7ab143da473316f25f911ebf094d9
Piyush Dolar <piyushdolar111@gmail.com>
2021-01-31 12:15:46 +0700

    Meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,12 @@ export default {
 		meta: [
 			{ charset: "utf-8" },
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
-			{ hid: "description", name: "description", content: "" }
+			{ hid: "description", name: "description", content: "" },
+			{
+
+				"http-equiv": "Content-Security-Policy", "content": "upgrade-insecure-requests"
+			}
+
 		],
 		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
 	},
