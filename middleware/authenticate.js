cb8b42ce605845e583fc5fe077053d6ef89d2fed
Piyush Dolar <piyushdolar111@gmail.com>
2021-03-04 19:02:04 +0700

    Fixed auth cookie problem

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,5 +2,8 @@ export default function (context) {
 	// If the user is not authenticated
 	if (!localStorage.token) {
 		return context.redirect(context.app.localePath('/login'));
+	} else if (!context.$cookie.get('user')) {
+		localStorage.token = '';
+		return context.redirect(context.app.localePath('/login'));
 	}
 }
