19c3ceea690ab47b46143fca7359ccc1ccbb325f
Piyush Dolar <piyushdolar111@gmail.com>
2021-04-20 17:40:48 +0700

    Card fixes and api fixes

--- components/players/profile.vue
+++ components/players/profile.vue
@@ -2,50 +2,33 @@
 	<div>
 		<v-row class="my-3">
 			<v-col cols="12" lg="12" md="12" sm="12">
-				<v-card class="mx-auto">
-					<v-app-bar dark>
-						<v-toolbar-title>{{
-							$t("loginIpPool")
-						}}</v-toolbar-title>
-						<v-spacer></v-spacer>
-					</v-app-bar>
-
-					<v-card-text>
-						<!-- Table Listing -->
-						<v-row>
-							<v-col cols="12">
-								<v-card outlined>
-									<v-card-text>
-										<v-data-table
-											dense
-											:headers="headers"
-											:items="
-												playerInfo.loginip.loginIpPool
-											"
-											:search="search"
-											item-key="logintime"
-											class="elevation-1"
-										>
-											<template
-												v-slot:[`item.logintime`]="{
-													item,
-												}"
-											>
-												{{
-													$moment(
-														item.logintime
-													).format(
-														"YYYY-MM-DD HH:mm:ss"
-													)
-												}}
-											</template>
-										</v-data-table>
-									</v-card-text>
-								</v-card>
-							</v-col>
-						</v-row>
-					</v-card-text>
-				</v-card>
+				<!-- Table Listing -->
+				<v-row>
+					<v-col cols="12">
+						<v-card outlined>
+							<v-card-title>{{ $t("loginIpPool") }}</v-card-title>
+							<v-card-text>
+								<v-data-table
+									dense
+									:headers="headers"
+									:items="playerInfo.loginip.loginIpPool"
+									:search="search"
+									item-key="logintime"
+								>
+									<template
+										v-slot:[`item.logintime`]="{ item }"
+									>
+										{{
+											$moment(item.logintime).format(
+												"YYYY-MM-DD HH:mm:ss"
+											)
+										}}
+									</template>
+								</v-data-table>
+							</v-card-text>
+						</v-card>
+					</v-col>
+				</v-row>
 			</v-col>
 		</v-row>
 	</div>
--- pages/system/players/_id.vue
+++ pages/system/players/_id.vue
@@ -26,20 +26,20 @@
 					playerId: this.$route.params.id,
 				},
 			});
-			// Clicks
-			this.$store.dispatch("playerInfo", {
-				type: "clicks",
-				params: {
-					playerId: this.$route.params.id,
-				},
-			});
-			// Views
-			this.$store.dispatch("playerInfo", {
-				type: "views",
-				params: {
-					playerId: this.$route.params.id,
-				},
-			});
+			// // Clicks
+			// this.$store.dispatch("playerInfo", {
+			// 	type: "clicks",
+			// 	params: {
+			// 		playerId: this.$route.params.id,
+			// 	},
+			// });
+			// // Views
+			// this.$store.dispatch("playerInfo", {
+			// 	type: "views",
+			// 	params: {
+			// 		playerId: this.$route.params.id,
+			// 	},
+			// });
 			// Profile
 			this.$store.dispatch("playerInfo", {
 				type: "info",
