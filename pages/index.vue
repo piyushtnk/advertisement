b68a6cb02640d165a197dc882303d5bf8e08dc2c
Piyush Dolar <piyushdolar111@gmail.com>
2021-04-23 15:57:30 +0700

    Updated the chagne of blank page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<div v-if="token">
+		<!-- <div v-if="token">
 			<p>
 				You are authenticated. You can login to system!
 				<NuxtLink :to="localePath('/system')"> Click Here </NuxtLink>
@@ -9,14 +9,14 @@
 		<p v-else>
 			Please
 			<NuxtLink :to="localePath('/login')"> login </NuxtLink>.
-		</p>
+		</p> -->
 	</div>
 </template>
 
 <script>
 	export default {
 		layout: "login",
-		middleware: "authenticate",
+		// middleware: "authenticate",
 		data() {
 			return {};
 		},
@@ -26,11 +26,11 @@
 			},
 		},
 		mounted() {
-			if (this.$cookie.get("token")) {
-				this.$router.push(this.localePath("/system"));
-			} else {
-				this.$router.push(this.localePath("/login"));
-			}
+			// if (this.$cookie.get("token")) {
+			// 	this.$router.push(this.localePath("/system"));
+			// } else {
+			// 	this.$router.push(this.localePath("/login"));
+			// }
 		},
 	};
 </script>
